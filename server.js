de8ce15fc06a6acd598be87dee4b6b304f4ae7e3
wekanda <wekandasamuel22@gmail.com>
2025-11-15 17:53:35 +0300

    fix: correct all file import paths definitively

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 
 // Import route files
-const authRoutes = require('./src/routes/auth');
-const projectRoutes = require('./src/routes/projects');
-const userRoutes = require('./src/routes/userRoutes');
+const authRoutes = require('./src/routes/auth.js');
+const projectRoutes = require('./src/routes/projects.js');
+const userRoutes = require('./src/routes/userRoutes.js');
 
 // Initialize dotenv
 dotenv.config();
