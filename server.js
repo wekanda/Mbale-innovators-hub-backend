043fb8e2e2efcd1214bf373b2af539de1c4090d7
wekanda <wekandasamuel22@gmail.com>
2025-11-15 18:26:13 +0300

    fix: align start command with Render root directory

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('../config/db');
+const connectDB = require('./config/db');
 
 // Import route files
 const authRoutes = require('./routes/auth');
