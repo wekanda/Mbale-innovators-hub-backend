aef8e72d00194ec15f56d384b460ac801dfbb045
wekanda <wekandasamuel22@gmail.com>
2025-11-15 18:43:25 +0300

    refactor: finalize project structure and fix all paths

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 
 // Import route files
-const authRoutes = require('./routes/auth');
-const projectRoutes = require('./routes/projects');
-const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./src/routes/auth');
+const projectRoutes = require('./src/routes/projects');
+const userRoutes = require('./src/routes/userRoutes');
 
 // Initialize dotenv
 dotenv.config();
